6a744a6540016ba2938bf467664ce10bc5c73665
Victor Estrade <victor.antoine.estrade@gmail.fr>
2016-05-06 12:14:33 +0200

    Remove plot_bound function and useless test as main

--- utils.py
+++ utils.py
@@ -44,61 +44,6 @@ def pop_last_line(fname):
     return line
 
 
-def plot_bound(X, y, predict_fn):
-    """
-    Plot the bounds of a 2D dataset (X,y) given a probability prediction
-    function.
-    """
-    from matplotlib.colors import ListedColormap
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, .02),
-                         np.arange(y_min, y_max, .02))
-    Z = predict_fn(np.c_[xx.ravel(), yy.ravel()])
-    Z = np.array(Z)[0, :, 1]
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    plt.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-
-    # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_bright)
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-
-
-def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
-    """
-    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(cm.shape[0])
-    plt.xticks(tick_marks, tick_marks, rotation=45)
-    plt.yticks(tick_marks, tick_marks)
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-
-def save_confusion_matrix(y_test, y_pred, title='Confusion matrix', cmap=plt.cm.Blues):
-    """
-    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-    """
-    from sklearn.metrics import confusion_matrix
-
-    cm = confusion_matrix(y_test, y_pred)
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    
-    plot_confusion_matrix(cm, title=title)
-
-    plt.savefig('fig/'+title+'.png', bbox_inches='tight')
-    plt.clf() # Clear plot window
-    plt.close() # Clear plot window
-
-
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     """
     Helper function interating over the given inputs
@@ -155,9 +100,3 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
         else:
             excerpt = slice(start_idx, start_idx + batchsize)
         yield tuple(arr[excerpt] for arr in data)
-
-
-if __name__ == '__main__':
-    print('I am at your service Master')
-    reverse_lines('TODO.txt')
-    print(pop_last_line('TODO.txt'))
