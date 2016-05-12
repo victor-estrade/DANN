Corrected normalization + add random permutation

datasets/toys.py
@@ -4,10 +4,10 @@ import numpy as np
 from datasets.utils import shuffle_array
 from sklearn.datasets import make_moons
 
+
 # ============================================================================
 #                   Moons
 # ============================================================================
-
 
 def load_moons(noise=0.05, n_samples=500, batchsize=32):
     """
@@ -177,7 +177,7 @@ def load_circles(n_samples=50 ,n_classes=5, batchsize=20):
         arr = np.random.normal(0, 1/n_classes, size=(n_samples, 2))
         rho = np.sqrt(arr[:, 0]**2 + arr[:, 0]**2)
         phi = np.arctan2(arr[:, 1], arr[:, 0])
-        rho = (p+1)/(1+np.exp(rho))
+        rho = (p+1)/(1+np.exp(rho))/n_classes
         arr[:, 0] = rho * np.cos(phi)
         arr[:, 1] = rho * np.sin(phi)
         return arr

datasets/transform.py
@@ -76,7 +76,7 @@ def diag_dataset(source_data, normalize=False):
     return target_data
 
 
-def random_mat_dataset(source_data, normalize=False):
+def random_mat_dataset(source_data, normalize=False, random_state=None):
     """
     Transform the given dataset by applying a random matrix to it.
 
@@ -105,7 +105,8 @@ def random_mat_dataset(source_data, normalize=False):
     y_t_val = y_val
     y_t_test = y_test
 
-    A = np.random.random((size, size))
+    rand_state = np.random.RandomState(random_state)
+    A = rand_state.random((size, size))
     if normalize:
         normalizer = Normalizer()
         X_t_train = normalizer.fit_transform(np.dot(X_train.reshape(-1, size), A)).reshape(X_train.shape)
@@ -272,3 +273,55 @@ def mirror_dataset(source_data, shape=(-1,28,28)):
 
     return target_data
 
+
+def random_permut_dataset(source_data, random_state=None):
+    """
+    Transform the given dataset by applying a simple operation to it.
+
+    target_data <- random_permutation of feature(source_data)
+
+    Params
+    ------
+        source_data: a dataset (dict with the separated data)
+
+    Return
+    ------
+        target_data: dict with the separated transformed data
+
+    """
+
+    X_train = np.copy(source_data['X_train'])
+    y_train = source_data['y_train']
+    X_val = np.copy(source_data['X_val'])
+    y_val = source_data['y_val']
+    X_test = np.copy(source_data['X_test'])
+    y_test = source_data['y_test']
+    batchsize = source_data['batchsize']
+
+    # Take care of 3D or n-D data
+    rand_state = np.random.RandomState(random_state)
+    permutation = rand_state.permutation(np.prod(X_train.shape[1:]))
+    
+    shape = X_train.shape
+    X_train = X_train.reshape(-1, np.prod(shape[1:]))
+    X_train = X_train[:, permutation].reshape(shape)
+
+    shape = X_val.shape
+    X_val = X_val.reshape(-1, np.prod(shape[1:]))
+    X_val = X_val[:, permutation].reshape(shape)
+
+    shape = X_test.shape
+    X_test = X_test.reshape(-1, np.prod(shape[1:]))
+    X_test = X_test[:, permutation].reshape(shape)
+
+    target_data = {
+                'X_train': X_train,
+                'y_train': y_train,
+                'X_val': X_val,
+                'y_val': y_val,
+                'X_test': X_test,
+                'y_test': y_test,
+                'batchsize': batchsize,
+                }
+
+    return target_data